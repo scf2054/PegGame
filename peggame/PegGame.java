4ef17db8186b63d40dc53965efaf17e598e1cd29
Sam Frost <sam@sams-mbp.wireless.rit.edu>
2021-03-19 08:49:04 -0400

    PegGame interface

---
+++ peggame/PegGame.java
@@ -0,0 +1,25 @@
+package peggame;
+
+import java.util.Collection;
+
+public interface PegGame {
+
+    public enum GameState {
+        NOT_STARTED, 
+        IN_PROGRESS,
+        STALEMATE,
+        WON;
+    }
+
+    public class PegGameException extends Exception {
+
+        public PegGameException(String message) throws PegGameException {
+            throw new PegGameException("Invalid move attempted.");
+        }
+    }
+
+    Collection<Move> getPossibleMoves();
+    GameState getGameState();
+    void MakeMove(Move move) throws PegGameException;
+    
+}
