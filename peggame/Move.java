baaade0524b210d22dabc328787889fb601085a1
NotRikkun <earthboundjesus77@gmail.com>
2021-03-19 08:38:17 -0400

    Added the Move class

---
+++ peggame/Move.java
@@ -0,0 +1,33 @@
+package peggame;
+
+public class Move {
+    private final Location from;
+    private final Location to;
+
+    public Move(Location from, Location to){
+        this.from = from;
+        this.to = to;
+    }
+
+    public Location getFrom(){return from;}
+    public Location getTo(){return to;}
+
+    @Override
+    public boolean equals(Object o){
+        if(o instanceof Move){
+            Move other = (Move) o;
+            return this.from == other.from && this.to == other.to;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        return("From: "+from+"\nTo: "+to);
+    }
+
+    @Override
+    public int hashCode(){
+        return toString().hashCode();
+    }
+}
