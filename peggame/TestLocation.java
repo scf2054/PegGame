9f987b86b4a9a2f2772b0f87dff53044a09b749d
NotRikkun <earthboundjesus77@gmail.com>
2021-03-19 09:48:29 -0400

    Tested Move and Location classes.

---
+++ peggame/TestLocation.java
@@ -0,0 +1,35 @@
+package peggame;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+import org.junit.platform.commons.annotation.Testable;
+
+@Testable
+public class TestLocation {
+    @Test
+    public void testEqualsValid(){
+        int row = 0;
+        int col = 0;
+        Location location1 = new Location(row, col);
+        Location location2 = new Location(row, col);
+        boolean expected = true;
+
+        boolean actual = location1.equals(location2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testEqualsInvalid(){
+        int row = 0;
+        int col = 0;
+        Location location1 = new Location(row, col);
+        Location location2 = new Location(row, col+1);
+        boolean expected = false;
+
+        boolean actual = location1.equals(location2);
+
+        assertEquals(expected, actual);
+    }
+}
---
+++ peggame/TestMove.java
@@ -0,0 +1,59 @@
+package peggame;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+import org.junit.platform.commons.annotation.Testable;
+
+@Testable
+public class TestMove {
+    
+    @Test
+    public void testFetFrom(){
+        Location from = new Location(0,0);
+        Location to = new Location(1,1);
+
+        Move move = new Move(from, to);
+        Location actual = move.getFrom();
+
+        assertEquals(from, actual);
+    }
+
+    @Test
+    public void testFetTo(){
+        Location from = new Location(0,0);
+        Location to = new Location(1,1);
+
+        Move move = new Move(from, to);
+        Location actual = move.getTo();
+
+        assertEquals(to, actual);
+    }
+
+    @Test
+    public void testEqualsValid(){
+        Location from = new Location(0,0);
+        Location to = new Location(1,1);
+        Move move1 = new Move(from, to);
+        Move move2 = new Move(from, to);
+        boolean expected = true;
+
+        boolean actual = move1.equals(move2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testEqualsInvalid(){
+        Location from = new Location(0,0);
+        Location to = new Location(1,1);
+        Location too = new Location(1,2);
+        Move move1 = new Move(from, to);
+        Move move2 = new Move(from, too);
+        boolean expected = false;
+
+        boolean actual = move1.equals(move2);
+
+        assertEquals(expected, actual);
+    }
+}
