87546fd47471335752955e381708eafee5d73056
NotRikkun <earthboundjesus77@gmail.com>
2021-03-20 14:36:46 -0400

    Implimented getPossibleMoves

--- peggame/RectangularBoard.java
+++ peggame/RectangularBoard.java
@@ -1,17 +1,21 @@
 package peggame;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class RectangularBoard implements PegGame {
 
     private final int rows;
     private final int cols;
     private final Location[] locations;
+    private final Set<Move> movesMade;
 
     public RectangularBoard(int rows, int cols) {
         this.rows = rows;
         this.cols = cols;
         this.locations = makeLocations();
+        this.movesMade = new HashSet<>();
     }
 
     /**
@@ -36,19 +40,101 @@ public class RectangularBoard implements PegGame {
 
     @Override
     public Collection<Move> getPossibleMoves() {
-        // TODO Auto-generated method stub
+        Set<Move> possible = new HashSet<>();
+        for(Location location : this.locations){
+            if(location.hasPeg()){
+                // verticle
+                // If current location plus 2 isn't off the board
+                // If the location 1 away from where we are has a peg
+                // If the location 2 away does not have one
+                    Location moveTo = getLocation(location.getRow()+2, location.getCol());
+                    Location target = getLocation(location.getRow()+1, location.getCol());
+                    if(moveTo != null && target.hasPeg() && !moveTo.hasPeg()){
+                        Move moveV = new Move(location, moveTo);
+                        possible.add(moveV);
+                    }
+                    
+                    moveTo = getLocation(location.getRow()-2, location.getCol());
+                    target = getLocation(location.getRow()-1, location.getCol());
+                    if(moveTo != null && target.hasPeg() && !moveTo.hasPeg()){
+                        Move moveV = new Move(location, moveTo);
+                        possible.add(moveV);
+                    }
+
+                // horizontal
+                    moveTo = getLocation(location.getRow(), location.getCol()+2);
+                    target = getLocation(location.getRow(), location.getCol()+1);
+                    if(moveTo != null && target.hasPeg() && !moveTo.hasPeg()){
+                        Move moveV = new Move(location, moveTo);
+                        possible.add(moveV);
+                    }
+                    
+                    moveTo = getLocation(location.getRow(), location.getCol()-2);
+                    target = getLocation(location.getRow(), location.getCol()-1);
+                    if(moveTo != null && target.hasPeg() && !moveTo.hasPeg()){
+                        Move moveV = new Move(location, moveTo);
+                        possible.add(moveV);
+                    }
+
+                // diagonal
+                    moveTo = getLocation(location.getRow()+2, location.getCol()+2);
+                    target = getLocation(location.getRow()+1, location.getCol()+1);
+                    if(moveTo != null && target.hasPeg() && !moveTo.hasPeg()){
+                        Move moveV = new Move(location, moveTo);
+                        possible.add(moveV);
+                    }
+                    
+                    moveTo = getLocation(location.getRow()-2, location.getCol()-2);
+                    target = getLocation(location.getRow()-1, location.getCol()-1);
+                    if(moveTo != null && target.hasPeg() && !moveTo.hasPeg()){
+                        Move moveV = new Move(location, moveTo);
+                        possible.add(moveV);
+                    }
+                    
+                    moveTo = getLocation(location.getRow()+2, location.getCol()-2);
+                    target = getLocation(location.getRow()+1, location.getCol()-1);
+                    if(moveTo != null && target.hasPeg() && !moveTo.hasPeg()){
+                        Move moveV = new Move(location, moveTo);
+                        possible.add(moveV);
+                    }
+                    
+                    moveTo = getLocation(location.getRow()-2, location.getCol()+2);
+                    target = getLocation(location.getRow()-1, location.getCol()+1);
+                    if(moveTo != null && target.hasPeg() && !moveTo.hasPeg()){
+                        Move moveV = new Move(location, moveTo);
+                        possible.add(moveV);
+                    }
+            }
+        }
+        
+        return null;
+    }
+
+    private Location getLocation(int row, int col){
+        for(Location location : this.locations){
+            if(location.getRow() == row && location.getCol() == col){
+                return location;
+            }
+        }
         return null;
     }
 
     @Override
     public GameState getGameState() {
-        // TODO Auto-generated method stub
+        // The game is won is there is only 1 peg on board
+        // The game is not started if movesMade.size() = 0
+        // The game is in progress if getPossible moves is not empty
+        // The game is stalemate if getPossible moves is empty and movesMade is not empty
+        
         return null;
     }
 
     @Override
     public void MakeMove(Move move) throws PegGameException {
-        // TODO Auto-generated method stub
+        Location from = move.getFrom();
+        Location to = move.getTo();
+
+
         
     }
 
