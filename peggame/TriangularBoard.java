5206534dbb6dad55286f8b63b4f85b581be64cd9
NotRikkun <earthboundjesus77@gmail.com>
2021-04-02 09:36:35 -0400

    added everything except deepCopy and toString

--- peggame/TriangularBoard.java
+++ peggame/TriangularBoard.java
@@ -17,8 +17,13 @@ public class TriangularBoard implements PegGame{
     }
 
     private Location[] makeLocations() {
-        return null;
+        int total = 0;
+        for(int i = 1; i<size; i++){
+            total += i;
+        }
+        Location[] locations = new Location[total];
     }
+
     @Override
     public Collection<Move> getPossibleMoves() {
         Collection<Move> possible = new HashSet<>();
@@ -52,14 +57,81 @@ public class TriangularBoard implements PegGame{
 
     @Override
     public GameState getGameState() {
-        // TODO Auto-generated method stub
-        return null;
+        if(movesMade.size() == 0){
+            // The game is not started if movesMade.size() = 0
+            return GameState.NOT_STARTED;
+        } else {
+            // The game is in progress if getPossible moves is not empty
+            Set<Move> possible = (Set<Move>) getPossibleMoves();
+            if(possible.size() != 0){
+                return GameState.IN_PROGRESS;
+            } else {
+                int numPegs = 0;
+                for(Location location : locations){
+                    if(location.hasPeg()){
+                        numPegs++;
+                    }
+                }
+                // The game is won is there is only 1 peg on board
+                // The game is stalemate if getPossible moves is empty and movesMade is not empty
+                if(numPegs > 1){
+                    return GameState.STALEMATE;
+                } else {
+                    return GameState.WON;
+                }
+            }
+        }
     }
 
     @Override
     public void MakeMove(Move move) throws PegGameException {
-        // TODO Auto-generated method stub
-        
+        Set<Move> possibleMoves = (Set<Move>)getPossibleMoves();
+        if (!possibleMoves.contains(move)) {
+            throw new PegGameException("Move is not possible");
+        }
+        else {
+            Location from = move.getFrom();
+            Location to = move.getTo(); 
+            movesMade.add(move);
+            // original location remove peg
+            Location location = getLocation(from.getRow(), from.getCol());
+            location.setPeg(false);
+
+            // new location add peg
+            location = getLocation(to.getRow(), to.getCol());
+            location.setPeg(true);
+            // remove peg in the middle
+                // x is from.cols
+                int midCol = from.getCol();
+                // y is from.rows
+                int midRow = from.getRow();
+
+                // if x + 2 is to.col 
+                if (midCol + 2 == to.getCol()) {
+                    // midCol = x + 1
+                    midCol += 1;
+                }
+                // else if x - 2 is to.col
+                else if (midCol - 2 == to.getCol()) {
+                    // midCol = x - 1
+                    midCol -= 1;
+                }
+                // if y + 2 is to.row
+                if (midRow + 2 == to.getRow()) {
+                        // midRow = y + 1
+                        midRow += 1;
+                }
+                // else if y - 2 is to.row
+                else if (midRow - 2 == to.getRow()) {
+                    // midRow = y - 1
+                    midRow -= 1;
+                }
+                
+                // get location at midRow, midCol
+                location = getLocation(midRow, midCol);
+                // set hasPeg to false
+                location.setPeg(false);
+        }       
     }
 
     public Location getLocation(int row, int col) {
