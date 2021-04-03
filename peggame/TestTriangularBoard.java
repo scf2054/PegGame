8542656e1b9e70827d1a5e4f5096e70ab8771635
Sam Frost <sam@Sams-MacBook-Pro.local>
2021-04-03 12:37:09 -0400

    makeMove tested

--- peggame/TestTriangularBoard.java
+++ peggame/TestTriangularBoard.java
@@ -1,5 +1,6 @@
 package peggame;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
@@ -37,5 +38,54 @@ public class TestTriangularBoard {
         // analyze
         assertEquals(expected, actual);
     }
-    
+
+    @Test
+    public void testMakeMove() {
+        // setup
+        PegGame board = new TriangularBoard(3);
+        Location[] locations = board.getLocations();
+        locations[0].setPeg(true);
+        locations[2].setPeg(true);
+        locations[4].setPeg(true);
+        Location from = locations[0];
+        Location to = locations[5];
+        Move move = new Move(from, to);
+        String expected = "  - \n - - \n- o o \n";
+
+        // invoke
+        try {
+            board.MakeMove(move);
+            String actual = board.toString();
+
+            // analyze
+            assertEquals(expected, actual);
+        } catch (PegGameException e) {
+            assertFalse(true);
+        }
+    }
+
+    @Test
+    public void testMakeMoveInvalid() {
+        // setup
+        PegGame board = new TriangularBoard(3);
+        Location[] locations = board.getLocations();
+        locations[0].setPeg(true);
+        locations[2].setPeg(true);
+        locations[4].setPeg(true);
+        Location from = locations[0];
+        Location to = locations[4];
+        Move move = new Move(from, to);
+        String expected = "  - \n - - \n- o o \n";
+
+        // invoke
+        try {
+            board.MakeMove(move);
+            String actual = board.toString();
+
+            // analyze
+            assertEquals(expected, actual);
+        } catch (PegGameException e) {
+            assertFalse(false);
+        }
+    }
 }
